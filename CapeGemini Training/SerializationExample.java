a2305f1343e2195bcd27f520592db441cb5ca3db
Sachin Mishra(Suraj) <123259575+sachinmishra28122003@users.noreply.github.com>
2026-02-28 14:28:46 +0530

    Add files via upload

---
+++ CapeGemini Training/SerializationExample.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+public class SerializationExample {
+    public static void main(String args[]) {
+        try{
+            FileOutputStream fout = new FileOutputStream("serail.bin");
+            ObjectOutputStream ous = new ObjectOutputStream(fout);
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println();
+    }
+}
+// Serializable is called marker interface
+class Employees implements Serializable {
+
+}
