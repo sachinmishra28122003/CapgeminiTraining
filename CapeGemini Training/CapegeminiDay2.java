4295081216d2960d880d49f740c10d335490b6ea
Sachin Mishra(Suraj) <123259575+sachinmishra28122003@users.noreply.github.com>
2026-03-10 20:24:18 +0530

    Delete CapeGemini Training directory

--- CapeGemini Training/CapegeminiDay2.java
+++
@@ -1,9 +0,0 @@
-public class CapegeminiDay2 {
-    public static void main(String[] args) {
-        int a[][] = {{1,2},{3,4}};
-        System.out.println(a);
-        Object  o = null;
-        System.out.println(o);
-    }
-}
-
